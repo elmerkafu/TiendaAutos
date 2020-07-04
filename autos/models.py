d5aa20774abcbb9d67a2e94178aad4ff5061b2cc
Fernando Lira <fernando@datatrust.pe>
2020-07-04 11:17:39 -0500

    cambios

--- autos/models.py
+++ autos/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Tipo(models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=50,blank=False, null=False)
+
+    def __str__(self):
+        return self.tipo
+
+class Marca(models.Model):
+    id = models.AutoField(primary_key=True)
+    marca = models.CharField(max_length=60,blank=False, null=False)
+
+    def __str__(self):
+        return self.marca
+
+class Modelo(models.Model):
+    id = models.AutoField(primary_key=True)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    modelo = models.CharField(max_length=60,blank=False, null=False)
+    cilindros = models.CharField(max_length=60,blank=False, null=False)
+    potencia = models.CharField(max_length=60,blank=False, null=False)
+    sistema_alimentacion = models.CharField(max_length=60,blank=False, null=False)
+    suspension_delantera = models.CharField(max_length=60,blank=False, null=False)
+    suspension_posterior = models.CharField(max_length=60,blank=False, null=False)
+    torque = models.CharField(max_length=60,blank=False, null=False)
+    transmision = models.CharField(max_length=60,blank=False, null=False)
+    frenos_delanteros = models.CharField(max_length=60,blank=False, null=False)
+    frenos_traseros = models.CharField(max_length=60,blank=False, null=False)
+
+    def __str__(self):
+        return self.modelo
+
+class Auto(models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE)
+    auto = models.CharField(max_length=60,blank=False, null=False)
+    anno = models.IntegerField(blank=False, null=False)
+    precio = models.FloatField(blank=False, null=False)
+    foto = models.ImageField(blank=False, null=False)
+
+    def __str__(self):
+        return self.auto
--- tienda_auto/settings.py
+++ tienda_auto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'autos'
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,13 @@ WSGI_APPLICATION = 'tienda_auto.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tienda_autos',
+        'USER': 'postgres',
+        'PASSWORD': 'liraq_07',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_URL =  '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
