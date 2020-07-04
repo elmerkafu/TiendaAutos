5ec00bceccab2acf55cd5a43bf173681ffbbf07b
unknown <kafupaucarpura5@gmail.com>
2020-07-04 15:09:06 -0500

    formularios

--- autos/urls.py
+++ autos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import TipoView, MarcaView, ModeloView, AutoView, login, InsertAuto, InsertModelo
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView
 
 
@@ -14,6 +15,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('login', login, name="login"),
     path("logout/", LogoutView.as_view(), name='logout'),
-    path("insert_auto/", InsertAuto.as_view(), name="insert.auto"),
+    path("insert_auto/", login_required(InsertAuto.as_view()), name="insert.auto"),
     path("insert_modelo/", InsertModelo.as_view(), name="insert.modelo")
 ]
