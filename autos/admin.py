87579ba9179cfbdec603bd7f7d468b6efe2febb4
unknown <kafupaucarpura5@gmail.com>
2020-07-04 11:53:40 -0500

    0.1

--- autos/admin.py
+++ autos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tipo, Marca, Modelo, Auto
 
 # Register your models here.
+
+admin.site.register(Tipo)
+admin.site.register(Marca)
+admin.site.register(Modelo)
+admin.site.register(Auto)
+
