e34db6221db1dc1e3af0bbddaa2a1388e50b9379
Fernando Lira <fernando@datatrust.pe>
2020-07-04 15:31:56 -0500

    update

--- autos/urls.py
+++ autos/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from .views import TipoView, MarcaView, ModeloView, AutoView, login
 from .views import InsertAuto, InsertModelo, InsertMarca, InsertTipo, home
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
 
 
 router = routers.DefaultRouter()
@@ -15,9 +16,9 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('login', login, name="login"),
     path("logout/", LogoutView.as_view(), name='logout'),
-    path("insert_auto/", InsertAuto.as_view(), name="insert.auto"),
-    path("insert_modelo/", InsertModelo.as_view(), name="insert.modelo"),
-    path("insert_marca/", InsertMarca.as_view(), name="insert.marca"),
-    path("insert_tipo/", InsertTipo.as_view(), name="insert.tipo"),
-    path("", home, name="index")
+    path("insert_auto/", login_required(InsertAuto.as_view()), name="insert.auto"),
+    path("insert_modelo/", login_required(InsertModelo.as_view()), name="insert.modelo"),
+    path("insert_marca/", login_required(InsertMarca.as_view()), name="insert.marca"),
+    path("insert_tipo/", login_required(InsertTipo.as_view()), name="insert.tipo"),
+    path("", login_required(home), name="index")
 ]
