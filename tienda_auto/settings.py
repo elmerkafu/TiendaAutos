b71bbb15a01db42bfdf91571ffa78ed3d8f0f5ca
unknown <kafupaucarpura5@gmail.com>
2020-07-04 11:20:47 -0500

    elmer

--- tienda_auto/settings.py
+++ tienda_auto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'autos',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/login/'
